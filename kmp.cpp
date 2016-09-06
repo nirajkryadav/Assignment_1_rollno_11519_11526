f8e126d61e432e041aa542bd5a02f1cbc9c26f3a
Ridhwanluthra <Ridhwancode@gmail.com>
2016-09-06 18:02:45 +0530

    finalizing everything, the program is now ready for submission

--- kmp.cpp
+++ kmp.cpp
@@ -1,10 +1,11 @@
-/**
+/*
     NLP Assignment
-    KMP1.cpp
-    Purpose: counts the no. of paragraphs in a text file and implements dictionary search using KMP algorithm
+    kmp.cpp
+    Purpose: counts the no. of paragraphs in a text file and
+    implements dictionary search using KMP algorithm.
     @author Niraj(roll no. 11519), Ridhwan(roll no. 11526)
     @version 2.1.3 6/9/16
-    ****  Make sure that the text file you choose is in the same folder as that of the algorithm else specify the complete path ****
+    ## the text file should be text.txt
 */
 #include <iostream>    //for cin and cout
 #include <fstream>    //for file handeling
@@ -12,69 +13,69 @@
 #include <sstream>    // for splitting line in words
 using namespace std;    //makeing it easy to use standard input output
 
-/**
-    Constructs a prefix table of given string.
-    @param {string} 
-    @param {integer} 
-    @param {integer} 
-    Description: given any string pattern,
-    pre_table[i] refers to the length of the longest suffix
+/*
+    Constructs an array from substring needed for kmp algorithm.
+    @param {string} substr - substring from with lps is to be computed
+    @param {integer} substr_length - maximum length of the substring
+    @param {integer*} lps - pointer to the array to be created
+    Description: given substring to be found,
+    lps[i] refers to the length of the longest suffix
     of substring pattern[0,i-1] that matches its prefix.
 */
 
 void get_lps(string substr, int substr_length, int *lps) {
-    int len = 0;
+    int len = 0;	//length of longest prefix suffix till now
     int substr_cnt;
 
-    lps[0] = 0;
+    lps[0] = 0;    //because no prefix can exist
     substr_cnt = 1;
 
     while (substr_cnt < substr_length) {
-        if (substr[substr_cnt] == substr[len]) {
+        if (substr[substr_cnt] == substr[len]) {   //if match increment both
         	len++;
             lps[substr_cnt] = len;
             substr_cnt++;
         } else {
-            if (len != 0) {
+            if (len != 0) {    //if useable prefix exists
             	len = lps[len-1];
-        	} else {
+        	} else {    //else start from scratch
             	lps[substr_cnt] = 0;
             	substr_cnt++;
             }
        }
     }
 }
-/**
-    Constructs a prefix table of given string.
-    @param {string} 
-    @param {string} 
-    @return {integer} 
-    Description: given any string pattern,
-    pre_table[i] refers to the length of the longest suffix
-    of substring pattern[0,i-1] that matches its prefix.
+/*
+    Executes kmp search on the text
+    @param {string} substr - the substring to be searched
+    @param {string} text - the text file from with to search
+    @return {integer} word_count - returns total words found
+    Description: given a text and substring to be found,
+    if the substring is found that word is printed,
+    also the word_count is incremented to count total words found
 */
 int execute_kmp_search(string substr, string text) {
     int lps[substr.length()];
     int substr_cnt  = 0;
-    int counter = 0;
-    get_lps(substr, substr.length(), lps);
+    int word_count = 0;
+    get_lps(substr, substr.length(), lps);    //creates lps array
 
-    istringstream splitting(text);
+    istringstream splitting(text);    //using sstream for word parsing
 
     int text_cnt = 0;
     do {
         string word;
-        splitting >> word;
+        splitting >> word;		// getting the next word in word
         while (text_cnt < word.length()) {
-        	if (substr[substr_cnt] == word[text_cnt]) {
+        	if (substr[substr_cnt] == word[text_cnt]) {//increment if match
             	substr_cnt++;
                 text_cnt++;
             }
 
-            if (substr_cnt == word.length()) {
+            if (substr_cnt == word.length()) { //print word if substr match
             	substr_cnt = lps[substr_cnt-1];
 	            cout<<word<<endl;
-	            counter++;
+	            word_count++;
 	            break;
             } else if (text_cnt < word.length() &&
                     substr[substr_cnt] != word[text_cnt]) {
@@ -88,11 +89,13 @@ int execute_kmp_search(string substr, string text) {
         substr_cnt = 0;
     } while(splitting);
     cout<<endl;
-    return counter;
+    return word_count;
 }
 
 
 int main() {
+	cout<<"The name of file you want to use is file.txt"<<endl;
+	cout<<"Make sure the file is in this directory."<<endl;
     ifstream fin("file.txt");
 
     //will not count first word and last line so initial value is 1
@@ -108,14 +111,16 @@ int main() {
     while(gate == 1) {
         cout<<endl;
     	cout<<endl;
-    	cout<<"Type 1 - find no. of lines, words and paragraphs in the file"<<endl;
+    	cout<<"Type 1 - find no. of lines, words and ";
+    	cout<<"paragraphs in the file"<<endl;
     	cout<<"Type 2 - dictionary search"<<endl;
-    	cout<<"Type 3 - probability first do dictionary search and you will get an option for probaility."<<endl;
+    	cout<<"Type 3 - probability first do dictionary search and ";
+    	cout<<"you will get an option for probaility."<<endl;
     	int input;
     	cin>>input;
     	fin.seekg(0);  //bring file pointer to the start of the file
         while(fin) {
-        	fin.get(ch);   //getting character and comparing it to " " and /n
+        	fin.get(ch);  //getting character and comparing it to " " and /n
             text += ch;
             if(ch==' ' ||ch=='\n') {
                 words++;    //counting no of words
@@ -131,23 +136,29 @@ int main() {
           }
     	switch(input) {
     		case 1:
-                cout<<"Lines="<<line<<"\nWords="<<words<<"\n"<<"paragraph="<<para<<endl;
+                cout<<"Lines= "<<line;
+                cout<<"\nWords= "<<words;
+                cout<<"\nparagraph= "<<para<<endl;
                 break;
 			case 2:
 				cout<<"Enter string to search"<<endl;
     			cin>>word;
     			cout<<endl;
-    			searched_word_count = execute_kmp_search(word, text);    //calling kmp search function
+    			//calling kmp search function
+    			searched_word_count = execute_kmp_search(word, text);
 				break;
             case 3:
 				cout<<"Enter string to search"<<endl;
     			cin>>word;
     			cout<<endl;
     			searched_word_count = execute_kmp_search(word, text);
-    			probability = (float)searched_word_count / (float)words;    //calculating the probability
-    			cout<<"The number of words found: "<<searched_word_count<<endl;
+    			//calculating the probability
+    			probability = (float)searched_word_count / (float)words;
+    			cout<<"The number of words ";
+    			cout<<"found: "<<searched_word_count<<endl;
     			cout<< "The Total number of words: "<< words<<endl<<endl;
-    			cout<<"The probability of words containing "<<word<<" is - "<<probability<<endl;
+    			cout<<"The probability of words containing "<<word;
+    			cout<<" is - "<<probability<<endl;
     			cout<<endl;
 				break;
 			default:
